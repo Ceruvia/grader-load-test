4d54cddf403892e7efc3791c9207182a3bdfcdb3
fawwazabrials <fawwazabrial.fas@gmail.com>
2025-06-19 01:37:14 +0700

    fix: remove panic to not disturb flow of test

--- internal/tests/sample.go
+++ internal/tests/sample.go
@@ -18,13 +18,15 @@ func TestSampleBlackbox(server *machinery.Server, useBuilder bool, sampleIdx int
 	elapsed := time.Since(start)
 
 	if err != nil {
-		panic(err)
+		log.Printf("ERROR IN GRADING: %s", err.Error())
+		return false, elapsed
 	}
 
 	reflectResult, _ := asyncResult.Get(10)
 
 	if err != nil {
-		panic(err)
+		log.Printf("ERROR IN GRADING: %s", err.Error())
+		return false, elapsed
 	}
 
 	tmp := reflectResult[0]
@@ -37,4 +39,35 @@ func TestSampleBlackbox(server *machinery.Server, useBuilder bool, sampleIdx int
 	}
 
 	return assertGradingResult(result, tasks.GetBlackboxExpectedResult(useBuilder, sampleIdx)), elapsed
+}
+
+func TestBlackbox(server *machinery.Server, useBuilder bool, sampleIdx int) (bool, models.GradingResult, time.Duration) {
+	signature := tasks.GetBlackboxSignature(useBuilder, sampleIdx)
+
+	start := time.Now()
+	asyncResult, err := server.SendTask(signature)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		log.Printf("ERROR IN GRADING: %s", err.Error())
+		return false, models.GradingResult{}, elapsed
+	}
+
+	reflectResult, _ := asyncResult.Get(10)
+
+	if err != nil {
+		log.Printf("ERROR IN GRADING: %s", err.Error())
+		return false, models.GradingResult{}, elapsed
+	}
+
+	tmp := reflectResult[0]
+
+	var result models.GradingResult
+
+	err = json.Unmarshal([]byte(tmp.String()), &result)
+	if err != nil {
+		log.Fatalf("Failed to parse JSON: %v", err)
+	}
+
+	return assertGradingResult(result, tasks.GetBlackboxExpectedResult(useBuilder, sampleIdx)), result, elapsed
 }
